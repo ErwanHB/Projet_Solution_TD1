2d0ba3589b52c9cf2732021dc959637a90cc55aa
GuillaumeBG <guillaume.bourg24@gmail.com>
2021-01-12 13:41:43 +0100

    test clone

--- TD1/Program.cs
+++ TD1/Program.cs
@@ -12,7 +12,7 @@ namespace TD1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            int a = 0;
+            int a = 0; //Commentaire
             Console.ReadLine();
         }
     }
